2eae5f1622773865c553e03690cfefacc57a301f
petrostreknelli <petrostreknelli@users.noreply.github.com>
2018-02-21 14:33:30 -0800

    Update Testing_Jenkins.py

--- Testing_Jenkins.py
+++ Testing_Jenkins.py
@@ -1,6 +1,9 @@
+import datetime
+now = datetime.datetime.now()
+
 fo = open("Testing Jenkins.txt", "wb")
 fo.write( "Hello! First line of testing for jenkins.\n");
 fo.write( "2nd line of testing for jenkins.\n");
-fo.write( "02/21/18 13:46\n");
-fo.write( "02/21/18 13:54\n");
+fo.write( str(now) + "\n");
+
 fo.close()
