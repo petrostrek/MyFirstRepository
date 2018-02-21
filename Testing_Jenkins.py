397c067b7aac9412e014bff29995e619d7264f37
petrostreknelli <petrostreknelli@users.noreply.github.com>
2018-02-21 13:46:42 -0800

    Update Testing_Jenkins.py

--- Testing_Jenkins.py
+++ Testing_Jenkins.py
@@ -1,4 +1,5 @@
 fo = open("Testing Jenkins.txt", "wb")
 fo.write( "Hello! First line of testing for jenkins.\n");
 fo.write( "2nd line of testing for jenkins.");
+fo.write( "02/21/18 13:46");
 fo.close()
