57efb11fc901ad27f3b37fccb5034fa1e5d0576a
petrostreknelli <petrostreknelli@users.noreply.github.com>
2018-02-21 14:42:38 -0800

    Update Testing_Jenkins.py

--- Testing_Jenkins.py
+++ Testing_Jenkins.py
@@ -6,5 +6,6 @@ fo.write( "Hello! First line of testing for jenkins.\n");
 fo.write( "2nd line of testing for jenkins.\n");
 fo.write( str(now) + "\n");
 fo.write( str(now) + "\n");
+fo.write( str(now) + "\n");
 
 fo.close()
