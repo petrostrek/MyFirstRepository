73c2704887ab9b6ef867ff19602347887ade45b6
Tony Petronelli <petrostreknelli@users.noreply.github.com>
2018-03-13 13:28:57 -0700

    Update Testing_Jenkins.py

--- Testing_Jenkins.py
+++ Testing_Jenkins.py
@@ -4,12 +4,6 @@ now = datetime.datetime.now()
 fo = open("Testing_Jenkins.txt", "w+")
 fo.write( "Hello! First line of testing for jenkins.\n");
 fo.write( "2nd line of testing for jenkins.\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
-fo.write( str(now) + "\n");
+
 
 fo.close()
